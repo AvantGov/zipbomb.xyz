f19e94741ed9a53cf102d4489f4e2238635c97e5
avantgov <alexcouts18@gmail.com>
2024-11-01 12:45:49 -0500

    initial commit with create vite boilerplate added and basic directories created and sorted

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+// depends
+import React from 'react';
+
+
+// CSS
+import './CSS/App.css'
+
+function App() {
+
+  return (
+    <div className='App' id='App'>
+      i am zipbomb.xyz
+    </div>
+  )
+}
+
+export default App
