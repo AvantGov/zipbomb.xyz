c7771fbc39a32ac8a3419a3e7d1aa400622e768d
Alex <44531619+AvantGov@users.noreply.github.com>
2024-12-17 11:11:37 -0600

    Update upcomingShows.js
    updating with Jan 4th show (visible)

--- client/src/data/upcomingShows.js
+++ client/src/data/upcomingShows.js
@@ -10,5 +10,16 @@ export const upcomingShows = [
         information: "$10 DONATION AT THE DOOR",
         url: "https://www.instagram.com/p/DCmj6wTp5N3/"
     },
+        {
+        id: "show_010425",
+        title: "whenthedustsettles w/ Badtz-Maru, Sublet, .zipbomb, At the Bottom of the Great Lakes",
+        date: "Jan 4th",
+        time: "7:00p",
+        display: true,
+        location: "ZAO MKE",
+        city: "Milwaukee, WI",
+        information: "$10 DONATION (NOTAFLOF)",
+        url: "https://www.instagram.com/p/DDbMURctx0U/"
+    },
 
 ]
