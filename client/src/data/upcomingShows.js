afa2dfbca7617168acc313590aecb7430a61c4ed
Alex <44531619+AvantGov@users.noreply.github.com>
2025-02-07 13:55:26 -0600

    Update upcomingShows.js
    removed past shows, updated pending data for march show

--- client/src/data/upcomingShows.js
+++ client/src/data/upcomingShows.js
@@ -1,36 +1,47 @@
 export const upcomingShows = [
-    {
-        id: "show_122924",
-        title: "The Loop Benefit show w/ nihilism, xablazex, .zipbomb, +MORE",
-        date: "Dec 29th",
-        time: "5:30p",
-        display: false,
-        location: "The Loop",
-        city: "Sun Prairie, WI",
-        information: "$10 DONATION AT THE DOOR",
-        url: "https://www.instagram.com/p/DCmj6wTp5N3/"
-    },
-    {
-        id: "show_010425",
-        title: "whenthedustsettles w/ Badtz-Maru, Sublet, .zipbomb, At the Bottom of the Great Lakes",
-        date: "Jan 4th",
-        time: "7:00p",
-        display: false,
-        location: "ZAO MKE",
-        city: "Milwaukee, WI",
-        information: "$10 DONATION (NOTAFLOF)",
-        url: "https://www.instagram.com/p/DDbMURctx0U/"
-    },
-    {
-        id: "show_011725",
-        title: ".zipbomb w/ litwax, at the bottom of the great lakes, all life gone",
-        date: "Jan 17th",
-        time: "7:00p",
-        display: true,
-        location: "The Loop",
-        city: "Sun Praire, WI",
-        information: "$10 DONATION (NOTAFLOF)",
-        url: "https://www.instagram.com/p/DDxRV6GpmYz/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
-    },
+    // {
+    //     id: "show_122924",
+    //     title: "The Loop Benefit show w/ nihilism, xablazex, .zipbomb, +MORE",
+    //     date: "Dec 29th",
+    //     time: "5:30p",
+    //     display: false,
+    //     location: "The Loop",
+    //     city: "Sun Prairie, WI",
+    //     information: "$10 DONATION AT THE DOOR",
+    //     url: "https://www.instagram.com/p/DCmj6wTp5N3/"
+    // },
+    // {
+    //     id: "show_010425",
+    //     title: "whenthedustsettles w/ Badtz-Maru, Sublet, .zipbomb, At the Bottom of the Great Lakes",
+    //     date: "Jan 4th",
+    //     time: "7:00p",
+    //     display: false,
+    //     location: "ZAO MKE",
+    //     city: "Milwaukee, WI",
+    //     information: "$10 DONATION (NOTAFLOF)",
+    //     url: "https://www.instagram.com/p/DDbMURctx0U/"
+    // },
+    // {
+    //     id: "show_011725",
+    //     title: ".zipbomb w/ litwax, at the bottom of the great lakes, all life gone",
+    //     date: "Jan 17th",
+    //     time: "7:00p",
+    //     display: true,
+    //     location: "The Loop",
+    //     city: "Sun Praire, WI",
+    //     information: "$10 DONATION (NOTAFLOF)",
+    //     url: "https://www.instagram.com/p/DDxRV6GpmYz/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+    // },
+    // {
+    //     id: "show_031125",
+    //     title: "chain of hatred w/ .zipbomb + more",
+    //     date: "March 11th",
+    //     time: "7:00p",
+    //     display: true,
+    //     location: "The Loop",
+    //     city: "Sun Praire, WI",
+    //     information: "$10 DONATION (NOTAFLOF)",
+    //     url: ""
+    // }
 
 ]
